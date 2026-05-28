fa12c36bcb1094cc3b6a48e5a3a41b2ac29125f6
David De Vargas <ddvargas@levi.com>
2026-05-28 12:35:15 -0500

    feat (projects/migrate-ds): apply changes to any file in a folder

--- projects/migrate-ds/main.ml
+++ projects/migrate-ds/main.ml
@@ -167,30 +167,50 @@ let process_file path write =
   end else
     print_endline "\n(Dry run _ pass -w to apply changes)"
 
+(* ------- Recursively find files with a given extension ------- *)
+
+let rec find_files dir ext =
+  Sys.readdir dir
+  |> Array.to_list
+  |> List.concat_map (fun entry ->
+      let path = Filename.concat dir entry in
+      if Sys.is_directory path then find_files path ext
+      else if Filename.extension path = ext then [path]
+      else [])
+
+
 (* ---------- Command-line arguments ---------- *)
 
-let filename = ref ""
-let write    = ref false
+let filename   = ref ""
+let folderpath = ref ""
+let ext        = ref ""
+let write      = ref false
+
 let version = "0.1.0"
 
 let rules = [
-  ("-f", Arg.Set_string filename, " file to read");
-  ("-w", Arg.Set write, " write changes back to the file");
+  ("-f", Arg.Set_string filename,   " file to read");
+  ("-p", Arg.Set_string folderpath, " folder path to read");
+  ("-x", Arg.Set_string ext,        " extension file to filter");
+  ("-w", Arg.Set write,             " write changes back to the file");
   ("-v", Arg.Unit (fun () -> print_endline version; exit 0), " print version and exit");
 ]
 
-let usage =  "usage: some -f <filename>"
+let usage =  "usage: some -f <filename>, or some -p <folderpath>"
 
 (* ---------- Entry point ---------- *)
 
 let () =
   Arg.parse (Arg.align rules) (fun _ -> ()) usage;
 
-  if !filename = "" then (
-    print_endline "Please pass -f <filename>";
+  if !filename = "" && !folderpath = "" then (
+    print_endline "Please pass -f <filename>, or -p <folderpath>";
     exit 1
   );
 
-  process_file !filename write
 
-  
+  let file_list = find_files !folderpath !ext in
+
+  List.iter (fun file ->
+    process_file file write
+  ) file_list
