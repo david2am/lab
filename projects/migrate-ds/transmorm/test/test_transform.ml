47ba9ece891b6e6a6fb6dd2389be260734c2d2df
David De Vargas <ddvargas@levi.com>
2026-06-02 15:51:25 -0500

    fix (project/migrate-ds): undo previous changes to mapping_pattern

--- projects/migrate-ds/transmorm/test/test_transform.ml
+++ projects/migrate-ds/transmorm/test/test_transform.ml
@@ -16,7 +16,10 @@ let process_line_tests () =
     "padding: $space-1000 $space-0;" (process_line "padding: $spacing-40 0;" 1);
   Alcotest.(check string)
     "has keyword and has several more old_values"
-    "padding: $space-0 $space-0 $space-050 $space-050;" (process_line "padding: 0px 0 2 2px;" 1)
+    "padding: $space-0 $space-0 $space-050 $space-050;" (process_line "padding: 0px 0 2 2px;" 1);
+  Alcotest.(check string)
+    "has keyword but value is not in mapping"
+    "margin-top: -10px;" (process_line "margin-top: -10px;" 1)
 
 let () =
   Alcotest.run "transform"
