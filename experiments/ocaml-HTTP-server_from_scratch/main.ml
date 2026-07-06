c03f9e7f8db5d035fed81aff80c9132f7e973147
david2am <dvccueter@gmail.com>
2026-07-06 16:15:29 -0500

    feat: print one line at a time

--- experiments/ocaml-HTTP-server_from_scratch/main.ml
+++ experiments/ocaml-HTTP-server_from_scratch/main.ml
@@ -2,15 +2,25 @@ let read_file file =
   let read_channel ic =
     let buffer = Bytes.create 8 in
 
-    let rec loop () =
+    let rec loop acc =
       match In_channel.input ic buffer 0 (Bytes.length buffer) with
       | 0 -> ()
       | n ->
-        print_string (Bytes.sub_string buffer 0 n);
-        loop ()
+        let sub_str = (Bytes.sub_string buffer 0 n) in
+        let index = sub_str |> String.find_first ~sub:"\n" |> Option.value ~default:(-1) in
+
+        if index <> (-1) then
+          let len = String.length sub_str in
+          let line = acc ^ String.sub sub_str 0 index in
+          
+          Printf.printf "read: %s\n" line;
+
+          loop (String.sub sub_str index (len - index))
+        else
+          loop (acc ^ sub_str)
     in
 
-    loop ()
+    loop ""
   in 
 
   try Ok (In_channel.with_open_bin file read_channel)
