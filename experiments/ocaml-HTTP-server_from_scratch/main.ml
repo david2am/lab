eb3c67a6f7549fdc4fdaa623464b8e4329d93d5f
david2am <dvccueter@gmail.com>
2026-07-07 08:16:11 -0500

    feat: get one line at a time

--- experiments/ocaml-HTTP-server_from_scratch/main.ml
+++ experiments/ocaml-HTTP-server_from_scratch/main.ml
@@ -2,31 +2,34 @@ let read_file file =
   let read_channel ic =
     let buffer = Bytes.create 8 in
 
-    let rec loop acc =
+    let rec get_lines acc lines =
       match In_channel.input ic buffer 0 (Bytes.length buffer) with
-      | 0 -> ()
+      | 0 ->
+        let final_lines = if String.length acc <> 0 then acc::lines else lines in
+        List.rev final_lines
       | n ->
-        let sub_str = (Bytes.sub_string buffer 0 n) in
-        let index = sub_str |> String.find_first ~sub:"\n" |> Option.value ~default:(-1) in
+        let sub_str = BytesLabels.sub_string buffer ~pos:0 ~len:n in
+        let index = sub_str
+          |> String.find_first ~sub:"\n"
+          |> Option.value ~default:(-1)
+        in
 
         if index <> (-1) then
           let len = String.length sub_str in
           let line = acc ^ String.sub sub_str 0 index in
           
-          Printf.printf "read: %s\n" line;
-
-          loop (String.sub sub_str index (len - index))
+          get_lines (StringLabels.sub sub_str ~pos:index ~len:(len - index)) (line::lines)
         else
-          loop (acc ^ sub_str)
+          get_lines (acc ^ sub_str) lines
     in
 
-    loop ""
+    get_lines "" []
   in 
 
-  try Ok (In_channel.with_open_bin file read_channel)
+  try  Ok (In_channel.with_open_bin file read_channel)
   with Sys_error msg -> Error msg
 
 let () =
   match read_file "message.txt" with
-  | Ok () -> ()
+  | Ok lines  -> List.iter (Printf.printf "read: %s\n") lines
   | Error msg -> Printf.eprintf "Error: %s\n" msg; exit 1
