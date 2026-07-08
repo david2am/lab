986334ba362941bfa3e4205fe0227ecb69006d2a
david2am <dvccueter@gmail.com>
2026-07-07 19:09:30 -0500

    refact: improve efficiency by using Buffer.t

--- experiments/ocaml-HTTP-server_from_scratch/main.ml
+++ experiments/ocaml-HTTP-server_from_scratch/main.ml
@@ -1,35 +1,45 @@
-let read_file file =
-  let read_channel ic =
-    let buffer = Bytes.create 8 in
-
-    let rec get_lines acc lines =
-      match In_channel.input ic buffer 0 (Bytes.length buffer) with
-      | 0 ->
-        let final_lines = if String.length acc <> 0 then acc::lines else lines in
-        List.rev final_lines
-      | n ->
-        let sub_str = BytesLabels.sub_string buffer ~pos:0 ~len:n in
-        let index = sub_str
-          |> String.find_first ~sub:"\n"
-          |> Option.value ~default:(-1)
-        in
-
-        if index <> (-1) then
-          let len = String.length sub_str in
-          let line = acc ^ String.sub sub_str 0 index in
-          
-          get_lines (StringLabels.sub sub_str ~pos:index ~len:(len - index)) (line::lines)
-        else
-          get_lines (acc ^ sub_str) lines
+let rec get_lines (acc : Buffer.t) lines buffer ic =
+  match In_channel.input ic buffer 0 (Bytes.length buffer) with
+  | 0 ->
+    (* EOF *)
+    let final_lines =
+      if Buffer.length acc <> 0 then
+        Buffer.contents acc :: lines
+      else
+        lines
     in
 
-    get_lines "" []
-  in 
+    List.rev final_lines
+  | n ->
+    let sub_str = BytesLabels.sub_string buffer ~pos:0 ~len:n in
+    let index = String.index_opt sub_str '\n' in
 
-  try  Ok (In_channel.with_open_bin file read_channel)
+    match index with
+    | None ->
+      Buffer.add_substring acc sub_str 0 n;
+      get_lines acc lines buffer ic
+    | Some idx ->
+      let line =
+        Buffer.add_substring acc sub_str 0 idx;
+        Buffer.contents acc
+      in
+      Buffer.clear acc; (* reset for next line *)
+
+      Buffer.add_substring acc sub_str (idx + 1) (n - idx - 1);
+
+      get_lines acc (line::lines) buffer ic
+
+
+let read_file file =
+  let acc = Buffer.create 100 in
+  let buffer = Bytes.create 8 in
+
+  try  Ok (
+    In_channel.with_open_bin file (get_lines acc [] buffer)
+  )
   with Sys_error msg -> Error msg
 
 let () =
   match read_file "message.txt" with
-  | Ok lines  -> List.iter (Printf.printf "read: %s\n") lines
+  | Ok  lines -> List.iter (Printf.printf "read: %s\n") lines
   | Error msg -> Printf.eprintf "Error: %s\n" msg; exit 1
